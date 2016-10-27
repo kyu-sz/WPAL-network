9e157f83a50564105a0506f83bca570db49e1cc0
Ken Yu <kyu_115s@hotmail.com>
2016-10-27 19:37:49 +0800

    Remove the three minor loss layers.

--- experiments/examples/test_mll_peta.sh
+++ experiments/examples/test_mll_peta.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-./tools/test_net.py --setid 0 --net ./data/snapshots/VGG_S_MLL_PETA/0/vgg_s_mll_peta_iter_28000.caffemodel --def ./models/VGG_S_MLL_PETA/test_net.prototxt --gpu 2 --db ProcessedPeta
+./tools/test_net.py --setid 0 --net ./data/snapshots/VGG_S_MLL_PETA/0/ProcessedPeta/vgg_s_mll_peta_iter_33000.caffemodel --def ./models/VGG_S_MLL_PETA/test_net.prototxt --gpu 3 --db ProcessedPeta
--- experiments/examples/train_mll_rap_0.sh
+++ experiments/examples/train_mll_rap_0.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 cd `dirname "${BASH_SOURCE[0]}"`/../..
-./experiments/scripts/wma_net.sh 0 VGG_S_MLL_RAP data/pretrained/VGG_CNN_S.caffemodel RAP 0
+./experiments/scripts/wma_net.sh 0 VGG_S_MLL_RAP data/snapshots/VGG_S_MLL_RAP/0/vgg_s_mll_rap_iter_35000.caffemodel RAP 0
