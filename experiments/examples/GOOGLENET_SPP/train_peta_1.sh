d0d2032df8f6ed8e79dee2e7da276211fb572c59
Ken Yu <kyu_115s@hotmail.com>
2016-11-08 19:22:40 +0800

    Use flexible SPP layer.

--- experiments/examples/GOOGLENET_SPP/train_peta_1.sh
+++ experiments/examples/GOOGLENET_SPP/train_peta_1.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 cd `dirname "${BASH_SOURCE[0]}"`/../../..
-./experiments/scripts/wpal_net.sh 3 GOOGLENET_SPP_PETA data/pretrained/bvlc_googlenet.caffemodel ProcessedPeta 1 --set TRAIN.BATCH_SIZE 32
+./experiments/scripts/wpal_net.sh 3 GOOGLENET_SPP_PETA data/pretrained/bvlc_googlenet.caffemodel ProcessedPeta 1 --set TRAIN.BATCH_SIZE 16
