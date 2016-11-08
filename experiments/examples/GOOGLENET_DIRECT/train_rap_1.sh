90a3739b0054a73e53af1ca0df37c7a1856abaaf
Ken Yu <kyu_115s@hotmail.com>
2016-11-08 15:21:27 +0800

    Remove useless models.

--- experiments/examples/GOOGLENET_DIRECT/train_rap_1.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-cd `dirname "${BASH_SOURCE[0]}"`/../../..
-./experiments/scripts/wpal_net.sh 2 GOOGLENET_RAP_DIRECT ./data/pretrained/bvlc_googlenet.caffemodel RAP 1 --set TRAIN.BATCH_SIZE 32
