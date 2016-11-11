16f18d7cb89734f1f61297d8c1b740c35ef9b15f
Ken Yu <kyu_115s@hotmail.com>
2016-11-11 07:13:56 -0500

    Adjust params.

--- experiments/examples/GOOGLENET_SPP/train_rap_0.sh
+++ experiments/examples/GOOGLENET_SPP/train_rap_0.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 cd `dirname "${BASH_SOURCE[0]}"`/../../..
-./experiments/scripts/wpal_net.sh 2 GOOGLENET_SPP_RAP ./data/snapshots/GOOGLENET_SPP_RAP/0/RAP/googlenet_spp_rap_iter_10000.caffemodel RAP 0 --set TRAIN.BATCH_SIZE 16
+./experiments/scripts/wpal_net.sh 2 GOOGLENET_SPP_RAP ./data/pretrained/bvlc_googlenet.caffemodel RAP 0 --set TRAIN.BATCH_SIZE 16
