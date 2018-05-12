59bfdbf6bfaf9af2408c70cbd820094f1af367bc
Awadhesh42 <38135852+Awadhesh42@users.noreply.github.com>
2018-05-12 17:00:20 +0530

    Create estimate_rap.sh

---
+++ experiments/examples/VGG_CNN_S/estimate_rap.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+cd `dirname "${BASH_SOURCE[0]}"`/../../..
+./tools/estimate_param.py --setid 0 --net ./data/snapshots/VGG_S_MLL_RAP/0/RAP/vgg_s_mll_rap_iter_30000.caffemodel --def ./models/VGG_S_MLL_RAP/test_net.prototxt --gpu 0 --db RAP
